7e5ee46ed4b691f5df6541297deb9dd291fa0c46
Henrik Barestrand <h.barestrand@gmail.com>
2026-02-17 19:17:39 +0100

    sync

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,13 @@ import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://barivia.se",
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en", "sv"],
+    routing: {
+      prefixDefaultLocale: false,
+    },
+  },
   integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
